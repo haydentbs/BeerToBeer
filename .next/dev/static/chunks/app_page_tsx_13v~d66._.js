64af82aedbe9288db2c7f17890e124d6364a1011
haydentbs <hayden.tibbals@gmail.com>
2026-03-20 22:56:24 +0000

    Stop tracking Next.js build artifacts

--- .next/dev/static/chunks/app_page_tsx_13v~d66._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0lz2uwg._.js","static/chunks/node_modules_0rsgu~4._.js"],
-    source: "dynamic"
-});
